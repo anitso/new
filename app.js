30784b78e0abc9c1aad4c247c764a4b55daa1941
annyts <90929385+annyts@users.noreply.github.com>
2021-10-21 18:14:29 +0400

    edit

---
+++ app.js
@@ -0,0 +1,54 @@
+// primitive types
+// string, number, boolean, null, undefined
+ 
+let str = "my name is "
+let str1 = 'my name is'
+let myName = "ani"
+
+console.log(str + myName)
+//sring contenation
+console.log(str + myName)
+//ES6
+console.log ('${str} ${myName} yep.');
+
+ let str = "my name is ani"
+ let myAge = 23;
+ let isTeenager = false;
+ let nothing= null;
+ let notDefined= undefined;
+ 
+
+var myName ="ani";
+console.log (box); 
+console.log (type str);
+console.log (typeof myAge);
+console.log (typeof isTeenager);
+console.log (typeof notDefined);
+
+let add = 70 + 30;
+let sub = 70 - 30;
+let multiply = 70 * 30;
+let div = 70 / 30
+
+console.log ('add - ',add)
+console.log ('sub - ',sub)
+console.log ('multiple - ', mutiple)
+console.log ('div - ', div)
+
+
+let length = 7;
+let wisth = 5;
+
+console.log ("the area =", length * width *);
+
+let firstSide = 5;
+let secondSide = 6;
+let thirdSide = 6;
+
+let semiPer = (firstSide * secondSide)/2
+
+
+let radius=7;
+console.log("The Area=", Math.PI* Math.pow(7,2));
+
+
